0b5af966e088e2c6bc5f53bb245bb0dfc90871f8
Nick Bumbarger <nbumbarger@gmail.com>
2016-09-08 15:05:11 -0400

    Adds dummy source data constants

---
+++ app/assets/scripts/constants.js
@@ -0,0 +1,23 @@
+
+export const mapSources = {
+  0: { // Represents admin 0, temporarily is NE States and Provinces
+    sourceLayer: 'ne_10m_admin_1_states_provinc-89mtkq',
+    idProp: 'scalerank',
+    url: 'mapbox://nbumbarg.07mallbu'
+  },
+  1: { // Represents admin 0, temporarily is NE Countries
+    sourceLayer: 'ne_10m_admin_0_countries-1mfz41',
+    idProp: 'scalerank',
+    url: 'mapbox://nbumbarg.ary69jed'
+  },
+  2: { // Represents admin 0, temporarily is NE Lakes
+    sourceLayer: 'ne_10m_lakes-a6ve5w',
+    idProp: 'scalerank',
+    url: 'mapbox://nbumbarg.ae5132j0'
+  },
+  3: { // Represents admin 0, temporarily is NE Urban Areas
+    sourceLayer: 'ne_50m_urban_areas-635qev',
+    idProp: 'scalerank',
+    url: 'mapbox://nbumbarg.b5h1mumk'
+  }
+}
