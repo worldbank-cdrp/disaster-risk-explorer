600af9b2ff3942e93f4b4ca0857f22d26368f607
Daniel da Silva <daniel@developmentseed.org>
2016-09-09 17:56:55 -0400

    Add dropdown component and initial styles.
    Add style utilities

--- app/assets/scripts/components/selection-panel.js
+++ app/assets/scripts/components/selection-panel.js
@@ -1,5 +1,6 @@
 import React from 'react'
 
+import Dropdown from './dropdown'
 import { } from '../actions'
 
 const Selection = React.createClass({
@@ -10,7 +11,19 @@ const Selection = React.createClass({
       <section className='selection'>
         <dl className='selection__panel'>
           <dt className='selection__panel--feature'>Risk</dt>
-          <dd className='selection__panel--drop'>Dropdown</dd>
+          <dd className='selection__panel--drop'>
+            <Dropdown
+              triggerElement='button'
+              triggerClassName='button button--base-unbounded drop__toggle--caret'
+              triggerTitle='Show/hide parameter options'
+              triggerText={'Hello'} >
+
+              <ul role='menu' className='drop__menu drop__menu--select'>
+                <li><a className={'drop__menu-item'} href='#' title='' data-hook='dropdown:close'><span>Value</span></a></li>
+              </ul>
+
+            </Dropdown>
+          </dd>
         </dl>
         <dl className='selection__panel'>
           <dt className='selection__panel--feature'>Risk</dt>
