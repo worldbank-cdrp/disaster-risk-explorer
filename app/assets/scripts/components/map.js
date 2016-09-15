2f39b847662208690804c7a5bd8a38002cef4106
Daniel da Silva <daniel@developmentseed.org>
2016-09-15 15:46:37 -0400

    Fix map selection issues

--- app/assets/scripts/components/map.js
+++ app/assets/scripts/components/map.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import mapboxgl from 'mapbox-gl'
 
-import { mapSources, inactiveLegend, hoverLegend } from '../constants'
+import { mapSources, inactiveLegends, hoverLegend } from '../constants'
 import { updateHovered, updateSelected } from '../actions'
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGV2c2VlZCIsImEiOiJnUi1mbkVvIn0.018aLhX0Mb0tdtaT2QNe2Q'
@@ -16,7 +16,18 @@ export const Map = React.createClass({
     selected: React.PropTypes.number
   },
 
+  getLegendStops: function (risk) {
+    return inactiveLegends[risk]
+  },
+
+  getColorProperty: function (risk) {
+    const columnMap = {earthquake: 'RP_10', hurricane: 'RP_500', flood: 'AAL'}
+    return columnMap[risk]
+  },
+
   componentDidMount: function () {
+    this.activeSource = mapSources[this.props.dataSelection.admin.getActive().key]
+    console.log('this.activeSource', this.activeSource)
     this.mapCenter = [-80, 15]
 
     const map = this._map = new mapboxgl.Map({
@@ -26,77 +37,121 @@ export const Map = React.createClass({
       zoom: 5,
       minZoom: 2
     })
+
     map.on('load', () => {
+      let risk = this.props.dataSelection.risk.getActive().key
       Object.keys(mapSources).forEach((id) => {
-        this.activeSource = mapSources[this.props.dataSelection.admin.getActive().key]
         const source = mapSources[id]
+        let visibility = this.activeSource.sourceLayer === source.sourceLayer ? 'visible' : 'none'
 
-        let visibility = 'none'
-        if (this.activeSource.sourceLayer === source.sourceLayer) visibility = 'visible'
+        this._map.addSource(id, {
+          type: 'vector',
+          url: source.url
+        })
 
-        this._addSource(false, source.id + '-inactive', source.url, source.sourceLayer,
-                       ['!=', source.id, ''], visibility, inactiveLegend, source.id)
-        this._addSource(false, id + '-hover', source.url, source.sourceLayer,
-                       ['==', source.id, ''], visibility, hoverLegend, source.id)
-        this._addSource(true, id + '-active', source.url, source.sourceLayer,
-                       ['==', source.id, ''], visibility)
-
-        map.on('mousemove', this._mouseMove)
-        map.on('click', this._mapClick)
+        this._addLayer(`${id}-inactive`, source.sourceLayer, id, ['!=', id, ''], visibility, this.getColorProperty(risk), this.getLegendStops(risk))
+        this._addLayer(`${id}-hover`, source.sourceLayer, id, ['==', id, ''], visibility, this.getColorProperty(risk), hoverLegend)
+        this._addOutlineLayer(`${id}-active`, source.sourceLayer, id, ['==', id, ''], visibility)
       })
+
+      map.on('mousemove', this._mouseMove)
+      map.on('click', this._mapClick)
     })
   },
 
   componentWillReceiveProps: function (nextProps) {
-    this.activeSource = mapSources[this.props.dataSelection.admin.getActive().key]
-    this._toggleSource(this.props.dataSelection.admin.getActive().key,
-                       nextProps.dataSelection.admin.getActive().key)
+    const prevSourceName = this.props.dataSelection.admin.getActive().key
+    const nextSourceName = nextProps.dataSelection.admin.getActive().key
+    if (nextSourceName !== prevSourceName) {
+      this._toggleSource(prevSourceName, nextSourceName)
+    }
 
     const prevColorProp = this.props.dataSelection.risk.getActive().key
     const nextColorProp = nextProps.dataSelection.risk.getActive().key
     if (nextColorProp !== prevColorProp) {
-      this._toggleLayerProperties(prevColorProp, nextColorProp)
+      this._toggleLayerProperties(prevColorProp, nextColorProp, prevSourceName, nextSourceName)
     }
+
+    // Done with switching. Update the active source
+    this.activeSource = mapSources[nextSourceName]
   },
 
-  _toggleLayerProperties: function (prevColorProp, nextColorProp) {
-    // const params = this.sources[this.activeSource]
-    // console.log(params)
-
-    // use dummy data field name mapping for testing
-    const dummyColumnMap = {earthquake: 'RP_10', hurricane: 'RP_25', flood: 'RP_50'}
-    nextColorProp = dummyColumnMap[nextColorProp]
-
-    const states = ['-inactive', '-hover', '-active']
-    states.forEach((layerState) => {
-      this._map.removeLayer(this.activeSource.id + layerState)
+  _mapClick: function (e) {
+    let sourceId = this.activeSource.id
+    const features = this._map.queryRenderedFeatures(e.point, {
+      layers: [`${sourceId}-inactive`, `${sourceId}-hover`]
     })
-
-    this._addLayer(this.activeSource.id + '-inactive', this.activeSource.sourceLayer,
-                  ['!=', this.activeSource.idProp, ''], 'visible', nextColorProp, inactiveLegend)
-    this._addLayer(this.activeSource.id + '-hover', this.activeSource.sourceLayer,
-                  ['==', this.activeSource.idProp, ''], 'none', nextColorProp, hoverLegend)
-    this._addOutlineLayer(this.activeSource.id + '-active', this.activeSource.sourceLayer,
-                         ['==', this.activeSource.idProp, ''], 'none')
-  },
-
-  _addSource: function (outline, id, url, layer, filter, visibility, colorScale, colorProperty) {
-    this._map.addSource(id, {
-      type: 'vector',
-      url: url
-    })
-    if (!outline) {
-      this._addLayer(id, layer, filter, visibility, colorProperty, colorScale)
+    if (features.length) {
+      this._selectFeature(features[0])
     } else {
-      this._addOutlineLayer(id, layer, filter, visibility)
+      this._deselectFeature()
     }
   },
 
-  _addLayer: function (id, layer, filter, visibility, colorProperty, colorScale) {
+  _selectFeature: function (feature) {
+    let propId = this.getColorProperty(this.props.dataSelection.risk.getActive().key)
+    this._map.setFilter(this.activeSource.id + '-active', ['==', propId, feature.properties[propId]])
+    // this.props.dispatch(updateSelected(feature))
+  },
+
+  _deselectFeature: function () {
+    let propId = this.getColorProperty(this.props.dataSelection.risk.getActive().key)
+    this._map.setFilter(this.activeSource.id + '-active', ['==', propId, ''])
+    // this.props.dispatch(updateSelected())
+  },
+
+  _mouseMove: function (e) {
+    let sourceId = this.activeSource.id
+    const features = this._map.queryRenderedFeatures(e.point, {
+      layers: [`${sourceId}-inactive`, `${sourceId}-hover`]
+    })
+    if (features.length) {
+      this._map.getCanvas().style.cursor = 'pointer'
+      this._highlightFeature(features[0])
+    } else {
+      this._map.getCanvas().style.cursor = ''
+      this._unhighlightFeature()
+    }
+  },
+
+  _highlightFeature: function (feature) {
+    let propId = this.getColorProperty(this.props.dataSelection.risk.getActive().key)
+    this._map.setFilter(this.activeSource.id + '-hover', ['==', propId, feature.properties[propId]])
+    // this.props.dispatch(updateHovered(feature))
+  },
+
+  _unhighlightFeature: function () {
+    let propId = this.getColorProperty(this.props.dataSelection.risk.getActive().key)
+    this._map.setFilter(this.activeSource.id + '-hover', ['==', propId, ''])
+    // this.props.dispatch(updateHovered())
+  },
+
+  _toggleSource: function (prevSource, nextSource) {
+    ['-inactive', '-hover', '-active'].forEach((type) => {
+      this._map.setLayoutProperty(prevSource + type, 'visibility', 'none')
+      this._map.setLayoutProperty(nextSource + type, 'visibility', 'visible')
+    })
+  },
+
+  _toggleLayerProperties: function (prevColorProp, nextColorProp, prevSourceName, nextSourceName) {
+    // Remove old layers
+    ['-inactive', '-hover', '-active'].forEach((type) => {
+      this._map.removeLayer(prevSourceName + type)
+    })
+
+    const nextSource = mapSources[nextSourceName]
+    let id = nextSource.id
+
+    this._addLayer(`${id}-inactive`, nextSource.sourceLayer, id, ['!=', id, ''], 'visible', this.getColorProperty(nextColorProp), this.getLegendStops(nextColorProp))
+    this._addLayer(`${id}-hover`, nextSource.sourceLayer, id, ['==', id, ''], 'visible', this.getColorProperty(nextColorProp), hoverLegend)
+    this._addOutlineLayer(`${id}-active`, nextSource.sourceLayer, id, ['==', id, ''], 'visible')
+  },
+
+  _addLayer: function (id, layer, source, filter, visibility, colorProperty, colorScale) {
     this._map.addLayer({
       'id': id,
       'type': 'fill',
-      'source': id,
+      'source': source,
       'source-layer': layer,
       'filter': filter,
       'layout': {
@@ -113,11 +168,11 @@ export const Map = React.createClass({
     })
   },
 
-  _addOutlineLayer: function (id, layer, filter, visibility) {
+  _addOutlineLayer: function (id, layer, source, filter, visibility) {
     this._map.addLayer({
       'id': id,
       'type': 'line',
-      'source': id,
+      'source': source,
       'source-layer': layer,
       'filter': filter,
       'paint': {
@@ -125,58 +180,6 @@ export const Map = React.createClass({
         'line-width': 3
       }
     })
-  },
-
-  _mapClick: function (e) {
-    const features = this._map.queryRenderedFeatures(e.point, {
-      layers: [this.activeSource.id + '-inactive', this.activeSource.id + '-hover'] })
-    if (features.length) {
-      this._selectFeature(features[0].properties[this.activeSource.idProp])
-    } else {
-      this._deselectFeature()
-    }
-  },
-
-  _mouseMove: function (e) {
-    const features = this._map.queryRenderedFeatures(e.point, {
-      layers: [this.activeSource.id + '-inactive', this.activeSource.id + '-hover'] })
-    if (features.length) {
-      this._map.getCanvas().style.cursor = 'pointer'
-      this._highlightFeature(features[0].properties[this.activeSource.idProp])
-    } else {
-      this._map.getCanvas().style.cursor = ''
-      this._unhighlightFeature()
-    }
-  },
-
-  _selectFeature: function (filter) {
-    this._map.setFilter(this.activeSource.id + '-active', ['==', this.activeSource.idProp, filter])
-    this.props.dispatch(updateSelected(filter))
-  },
-
-  _deselectFeature: function () {
-    this._map.setFilter(this.activeSource.id + '-active', ['==', this.activeSource.idProp, ''])
-    this.props.dispatch(updateSelected(0))
-  },
-
-  _highlightFeature: function (filter) {
-    this._map.setFilter(this.activeSource.id + '-hover', ['==', this.activeSource.idProp, filter])
-    this.props.dispatch(updateHovered(filter))
-  },
-
-  _unhighlightFeature: function () {
-    console.log(this.activeSource)
-    this._map.setFilter(this.activeSource.id + '-hover', ['==', this.activeSource.idProp, ''])
-    this.props.dispatch(updateHovered(0))
-  },
-
-  _toggleSource: function (prevSource, nextSource) {
-    if (nextSource !== prevSource) {
-      ['-inactive', '-hover', '-active'].forEach((source) => {
-        this._map.setLayoutProperty(nextSource + source, 'visibility', 'visible')
-        this._map.setLayoutProperty(prevSource + source, 'visibility', 'none')
-      })
-    }
   },
 
   render: function () {
--- app/assets/scripts/constants.js
+++ app/assets/scripts/constants.js
@@ -22,29 +22,46 @@ export const mapSources = {
 }
 
 const inactiveScale = chroma.scale(['rgb(240, 240, 240)', 'rgb(125, 125, 125)'])
-export const inactiveLegend = [
-  [0, inactiveScale(0).hex()],
-  [10, inactiveScale(0.1).hex()],
-  [20, inactiveScale(0.2).hex()],
-  [30, inactiveScale(0.3).hex()],
-  [40, inactiveScale(0.4).hex()],
-  [50, inactiveScale(0.5).hex()],
-  [60, inactiveScale(0.6).hex()],
-  [70, inactiveScale(0.7).hex()],
-  [80, inactiveScale(0.8).hex()],
-  [90, inactiveScale(0.8).hex()],
-  [100, inactiveScale(0.9).hex()]]
+const hoverScale = chroma.scale(['rgb(240, 255, 255)', 'rgb(155, 195, 195)'])
 
-let hoverScale = chroma.scale(['rgb(240, 255, 255)', 'rgb(155, 195, 195)'])
+const hurricaneLegend = [
+ [5000000, inactiveScale(0).hex()],
+ [5200000, inactiveScale(0.2).hex()],
+ [5400000, inactiveScale(0.4).hex()],
+ [5600000, inactiveScale(0.6).hex()],
+ [5800000, inactiveScale(0.8).hex()],
+ [6000000, inactiveScale(1).hex()]
+]
+
+const earthquakeLegend = [
+ [20500000, inactiveScale(1).hex()],
+ [75000000, inactiveScale(0.3).hex()],
+ [165000000, inactiveScale(0.2).hex()],
+ [320000000, inactiveScale(0.8).hex()],
+ [620000000, inactiveScale(0.4).hex()],
+ [6500000000, inactiveScale(0.7).hex()]
+]
+
+const floodLegend = [
+ [20500000, inactiveScale(0.4).hex()],
+ [75000000, inactiveScale(0.3).hex()],
+ [165000000, inactiveScale(0.1).hex()],
+ [320000000, inactiveScale(0.8).hex()],
+ [620000000, inactiveScale(0.7).hex()],
+ [6500000000, inactiveScale(0.9).hex()]
+]
+
+export const inactiveLegends = {
+  hurricane: hurricaneLegend,
+  earthquake: earthquakeLegend,
+  flood: floodLegend
+}
+
 export const hoverLegend = [
-  [0, hoverScale(0).hex()],
-  [10, hoverScale(0.1).hex()],
-  [20, hoverScale(0.2).hex()],
-  [30, hoverScale(0.3).hex()],
-  [40, hoverScale(0.4).hex()],
-  [50, hoverScale(0.5).hex()],
-  [60, hoverScale(0.6).hex()],
-  [70, hoverScale(0.7).hex()],
-  [80, hoverScale(0.8).hex()],
-  [90, hoverScale(0.8).hex()],
-  [100, hoverScale(0.9).hex()]]
+ [20500000, hoverScale(0.4).hex()],
+ [75000000, hoverScale(0.3).hex()],
+ [165000000, hoverScale(0.1).hex()],
+ [320000000, hoverScale(0.8).hex()],
+ [620000000, hoverScale(0.7).hex()],
+ [6500000000, hoverScale(0.9).hex()]
+]
