e86014975e6fda76f23b17c350be577ffdeaa701
Nick Bumbarger <nbumbarger@gmail.com>
2016-09-16 10:11:06 -0400

    Fixes test failures

--- test/components/map.js
+++ test/components/map.js
@@ -3,13 +3,15 @@ import test from 'ava'
 import { shallow } from 'enzyme'
 import proxyquire from 'proxyquire'
 import mockboxGL from 'mapbox-gl-js-mock'
+import DataSelection from '../../app/assets/scripts/utils/data-selection'
 
 const { Map } = proxyquire.noCallThru().load('../../app/assets/scripts/components/map', {
   'mapbox-gl': mockboxGL
 })
 
 test('map test', t => {
-  const component = shallow(<Map />)
+  const dataSelection = DataSelection({})
+  const component = shallow(<Map dataSelection={dataSelection} />)
 
   t.truthy(component.hasClass('map'))
 
