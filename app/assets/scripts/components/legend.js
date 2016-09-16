2f9d33fab65ee501ca3cbce43fe966d9db6c0cba
Nick Bumbarger <nbumbarger@gmail.com>
2016-09-16 16:02:38 -0400

    Add labels for each category; rather than only smallest and largest

--- app/assets/scripts/components/legend.js
+++ app/assets/scripts/components/legend.js
@@ -5,6 +5,7 @@ const Legend = React.createClass({
   propTypes: {
     dataSelection: React.PropTypes.object
   },
+
   capitalizeFirstLetter: function (string) {
     return string.charAt(0).toUpperCase() + string.slice(1)
   },
@@ -12,11 +13,13 @@ const Legend = React.createClass({
   shortenNumber: function (number, decimals) {
     decimals = (decimals === undefined) ? 0 : decimals
     if (number >= 1000000000) {
-      return `${Number((number / 1000000000).toFixed(decimals)).toLocaleString()} B`
+      return `${Number((number / 1000000000).toFixed(decimals))} B`
     } else if (number >= 1000000) {
-      return `${Number((number / 1000000).toFixed(decimals)).toLocaleString()} M`
+      return `${Number((number / 1000000).toFixed(decimals))} M`
+    } else if (number >= 1000) {
+      return `${Number((number / 1000).toFixed(decimals))} K`
     } else {
-      return number.toLocaleString()
+      return number
     }
   },
 
@@ -24,23 +27,28 @@ const Legend = React.createClass({
     const activeRisk = this.props.dataSelection.risk.getActive().key
     let legend = inactiveLegends[activeRisk]
 
-    const lowest = this.shortenNumber(legend[0][0], 3)
-    const highest = this.shortenNumber(legend[legend.length - 1][0], 3)
-
-    legend = legend.map((cat, i) => {
+    const legendBlocks = legend.map((cat, i) => {
       return (<span key={i}
                     className='legend__category'
                     style={{width: 100 / legend.length + '%',
-                            backgroundColor: cat[1]}}></span>
+                            backgroundColor: cat[1]}}>
+             </span>
+      )
+    })
+    const legendLabels = legend.map((cat, i) => {
+      return (<span key={i}
+                    className='legend__label'
+                    style={{width: 100 / legend.length + '%'}}>
+                      {this.shortenNumber(cat[0])}
+             </span>
       )
     })
     return (
       <section className='legend'>
 				<h2 className='legend__title'>Average Annual Loss from {this.capitalizeFirstLetter(activeRisk)}</h2>
 				<figure className='legend__scale'>
-          {legend}
-          <span className='legend__labels'>{lowest}</span>
-          <span className='legend__labels'>{highest}</span>
+          {legendBlocks}
+          {legendLabels}
         </figure>
 				<figcaption className='legend__caption'>The Average Annual Loss is not weighted by population. Therefore areas with greater AAL may not necessarily be more at risk for earthquakes.</figcaption>
       </section>
