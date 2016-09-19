21738ba684e6025ec49bf2c018bea85769348202
Dylan <Dylan@developmentseed.org>
2016-09-19 17:52:49 -0400

    Add a place for processing scripts, and the script for merging centroids onto the polygons

---
+++ app/assets/scripts/data-processing/merge-centroid-to-polygon.js
@@ -0,0 +1,44 @@
+#!/usr/bin/env node
+
+// Needs to run on node v6+
+
+var fs = require('fs')
+var rbush = require('rbush')
+var bb = require('turf-bbox')
+
+var pointLayer = JSON.parse(fs.readFileSync('CA-equake-centroids.geojson'))
+var gridLayer = JSON.parse(fs.readFileSync('Building_Stock.geojson'))
+
+var tree = rbush()
+tree.load(gridLayer.features.map(feature => {
+  const [minX, minY, maxX, maxY] = bb(feature)
+  return Object.assign({}, feature, { minX, minY, maxX, maxY })
+}))
+
+const geojsonOutput = {
+  properties: {},
+  type: 'FeatureCollection',
+  features: []
+}
+
+const mergedFeatures = pointLayer.features.map(feature => {
+  const [minX, minY, maxX, maxY] = bb(feature)
+  const searchResults = tree.search({minX, minY, maxX, maxY})
+
+  const match = searchResults[0]
+
+
+  const mergedProperties = Object.assign({}, match.properties, feature.properties)
+
+  // console.log(mergedProperties)
+
+  // console.log(Object.assign({}, match, {properties: mergedProperties}))
+
+  return Object.assign({}, match, {properties: mergedProperties})
+})
+
+console.log(mergedFeatures)
+
+geojsonOutput.features = mergedFeatures
+
+fs.writeFileSync('output.geojson', JSON.stringify(geojsonOutput))
