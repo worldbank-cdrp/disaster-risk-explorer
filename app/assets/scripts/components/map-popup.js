cc85b82bc492398779cb0e194bc4a6b0d03d617f
Dylan <Dylan@developmentseed.org>
2016-09-20 10:25:34 -0400

    Touchup and grabbing classes to help style the popups.

--- app/assets/scripts/components/map-popup.js
+++ app/assets/scripts/components/map-popup.js
@@ -7,16 +7,16 @@ var MapPopup = React.createClass({
   propTypes: {
     country: React.PropTypes.string
   },
-
   render: function () {
     return (
       <article className='popover'>
         <div className='popover__contents'>
           <header className='popover__header'>
-            <h1 className='popover__title'>{this.props.country}</h1>
+            <h1 className='subtitle subtitle__popover popover__title'>{this.props.country}</h1>
           </header>
           <div className='popover__body'>
-            <p>Good things come to those who wait...</p>
+            <p>Average Annual Loss</p>
+            <p className='popover__stat'>$610 Million</p>
           </div>
           <footer className='popover__footer'>
           </footer>
