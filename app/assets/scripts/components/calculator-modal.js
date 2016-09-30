7f22ecf695d10f9eb2687ae0d8b9f98cd4a133b7
Dylan <Dylan@developmentseed.org>
2016-09-30 09:34:29 -0400

    Finish adding in initial content, needs styling & hookup to state data

--- app/assets/scripts/components/calculator-modal.js
+++ app/assets/scripts/components/calculator-modal.js
@@ -23,7 +23,7 @@ const Calculator = React.createClass({
         <div className='modal__inner'>
           <header className='modal__header'>
             <div className='modal__headline'>
-              <h1 className='modal__title'>About this Tool</h1>
+              <h1 className='modal__title'>Risk mitigation cost and benefit calculation</h1>
               <button className='modal__button-dismiss' title='Close' onClick={() => this.props.dispatch(hideModalCalc())}><span>Dismiss</span></button>
             </div>
           </header>
@@ -41,14 +41,14 @@ const Calculator = React.createClass({
                 <button className='button header__language--toggle button__rightside'><span className='header__language--text'>Replace</span></button>
               </dl>
               <dl className='calc__selection'>
-                <dd>Cost per conversion</dd>
+                <dd>Cost per Replacement</dd>
                 <dt>$2,500</dt>
               </dl>
               <br></br>
               <h2 className='subtitle calc__subtitle'>Building Stock Converted</h2>
               <div className='calculator__description top'>Unreinforced Concrete Block/Fire Brick Masonry</div>
               <div className='calculator__divider-broken left'></div>
-              <div className='calculator__divider-broken-label'>are converted into</div>
+              <div className='calculator__divider-broken-label'>are replaced with</div>
               <div className='calculator__divider-broken right'></div>
               <div className='calculator__description bottom'>Reinforced masonry bearing walls with concrete diaphragms</div>
             </div>
@@ -56,35 +56,34 @@ const Calculator = React.createClass({
             <div className='modal__right-side'>
               <h2 className='subtitle calc__subtitle'>Results</h2>
               <dl className='calc_selection'>
-                <dd>Reduction of overall AAL</dd>
-                <dt>$10 Million</dt>
-                <dd>Total conversion cost</dd>
-                <dt>$150</dt>
-                <dd>Flat rate years to break even</dd>
-                <dt>255</dt>
-                <dd>Percent of total building stock covered</dd>
-                <dt>1%</dt>
-                <dd>Change in AAL for these buildings</dd>
-                <dt>21%</dt>
-                <dd>Change in overall AAL</dd>
-                <dt>10%</dt>
+                <dd className='stat__attribute'>Reduction of overall AAL</dd>
+                <dt className='stat__value'>$10 Million</dt>
+                <dd className='stat__attribute'>Total replacement cost</dd>
+                <dt className='stat__value'>$150</dt>
+                <dd className='stat__attribute'>Flat rate years to break even</dd>
+                <dt className='stat__value'>255</dt>
+                <dd className='stat__attribute'>Percent of Housing Stock replaced</dd>
+                <dt className='stat__value'>1%</dt>
+                <dd className='stat__attribute'>Percent Change in AAL for these housing units</dd>
+                <dt className='stat__value'>21%</dt>
+                <dd className='stat__attribute'>Change in overall AAL</dd>
+                <dt className='stat__value'>10%</dt>
               </dl>
 
               <br></br>
 
-              <h2 className='subtitle calc__subtitle'>Building Stock types most at risk</h2>
-              <h3 className=''>% of total AAL</h3>
+              <h2 className='subtitle calc__subtitle'>Building Stock types most at risk (absolute AAL)</h2>
               <dl className='calc_selection'>
-                <dd>Unreinforced Concrete Block/Fire Brick Masonry</dd>
-                <dt>10%</dt>
-                <dd>Unreinforced Concrete Block/Fire Brick Masonry</dd>
-                <dt>10%</dt>
-                <dd>Unreinforced Concrete Block/Fire Brick Masonry</dd>
-                <dt>10%</dt>
-                <dd>Unreinforced Concrete Block/Fire Brick Masonry</dd>
-                <dt>10%</dt>
-                <dd>Unreinforced Concrete Block/Fire Brick Masonry</dd>
-                <dt>10%</dt>
+                <dd className='stat__attribute'>Unreinforced Concrete Block/Fire Brick Masonry</dd>
+                <dt className='stat__value'>10%</dt>
+                <dd className='stat__attribute'>Unreinforced Concrete Block/Fire Brick Masonry</dd>
+                <dt className='stat__value'>10%</dt>
+                <dd className='stat__attribute'>Unreinforced Concrete Block/Fire Brick Masonry</dd>
+                <dt className='stat__value'>10%</dt>
+                <dd className='stat__attribute'>Unreinforced Concrete Block/Fire Brick Masonry</dd>
+                <dt className='stat__value'>10%</dt>
+                <dd className='stat__attribute'>Unreinforced Concrete Block/Fire Brick Masonry</dd>
+                <dt className='stat__value'>10%</dt>
               </dl>
 
             </div>
