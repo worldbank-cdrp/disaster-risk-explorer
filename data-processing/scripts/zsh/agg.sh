46bf6efa4eb02fc939e9da5fb761f9fecf105510
Drew Bollinger <drew@developmentseed.org>
2016-10-05 18:16:39 -0400

    Update paths

--- data-processing/scripts/zsh/agg.sh
+++ data-processing/scripts/zsh/agg.sh
@@ -1,23 +1,23 @@
 #!/bin/zsh
-BASE=../CDRP\ Platform\ Development\ Seed/
-for country in $0:h/countries/[A-Z][A-Z].geojson; do
+BASE=../CDRP\ Platform\ Development\ Seed
+for country in countries/[A-Z][A-Z].geojson; do
   echo country: ${country:t:r}
   for geo in $BASE/Hazard/Earthquake/probabilistic/${country:t:r}*_clipped.geojson; do
-       ./agg.js $geo $0:h/countries/${country:t:r}_grid.geojson
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   for geo in $BASE/Hazard/Windstorm/probabilistic/${country:t:r}*_clipped.geojson; do
-       ./agg.js $geo $0:h/countries/${country:t:r}_grid.geojson
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   for geo in $BASE/Hazard/Flood/probabilistic/${country:t:r}*_clipped.geojson; do
-       ./agg.js $geo $0:h/countries/${country:t:r}_grid.geojson
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   for geo in $BASE/Exposure/GDP/${country:t:r}_gdp.geojson; do
-       ./agg.js $geo $0:h/countries/${country:t:r}_grid.geojson
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   for geo in $BASE/Exposure/Infrastructure/${country:t:r}_infrastructure.geojson; do
-       ./agg.js $geo $0:h/countries/${country:t:r}_grid.geojson
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   for geo in $BASE/Exposure/Building\ Stock/${country:t:r}_building_stock.geojson; do
-       ./agg.js $geo $0:h/countries/${country:t:r}_grid.geojson
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
 done
