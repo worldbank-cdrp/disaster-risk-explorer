3e3a155209c27de5c39eeff34451fd7e7ea13a63
Dylan <Dylan@developmentseed.org>
2016-10-11 16:41:26 -0400

    Add some styling and content for the about section.

--- app/assets/scripts/components/about-modal.js
+++ app/assets/scripts/components/about-modal.js
@@ -23,19 +23,45 @@ const About = React.createClass({
         <div className='modal__inner'>
           <header className='modal__header'>
             <div className='modal__headline'>
-              <h1 className='modal__title'>About this Tool</h1>
+              <h1 className='modal__title'>About the Disaster Risk Explorer</h1>
               <button className='modal__button-dismiss' title='Close' onClick={() => this.props.dispatch(hideModalAbout())}><span>Dismiss</span></button>
             </div>
           </header>
-          <div className='modal__body'>
-            <h3>Section Header</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-            <h3>Section Header</h3>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?
-            </p>
-          </div>
+          <section className='modal__body'>
+            <div className='modal__body--about'>
+              <h3>What is this?</h3>
+              <p>
+                This tool allows you to explore and visualize Earthquake, Windstorm, & Flood data provided by the World Bank.
+              </p>
+              <h3>What Can I Explore?</h3>
+              <p>
+                These Risks can be viewed and assessed by the following metrics:
+              </p>
+              <dl>
+                <p><i className='collecticon collecticon-chart-line' /> Metrics</p>
+                <dt>Risk</dt>
+                <dd>The probability of the risk occuring.</dd>
+                <dt>Loss</dt>
+                <dd>The potential cost of disasters.</dd>
+                <dt>Exposure</dt>
+                <dd>The potential amount of infastructure affected.</dd>
+              </dl>
+              <dl>
+                <p><i className='collecticon collecticon-calendar' />Return Period</p>
+                <dt>Return Periods</dt>
+                <dd>The forecasted accumulated potential risk for a given amount of years.</dd>
+              </dl>
+              <dl>
+                <p><i className='collecticon-select-by' />Select Data By</p>
+                <dt>Country</dt>
+                <dd>View the data by Central American countries.</dd>
+                <dt>Admin One</dt>
+                <dd>View the data by Central American districts and states.</dd>
+                <dt>10km Grids</dt>
+                <dd>View the data by a 10km grid that covers the Central American area.</dd>
+              </dl>
+            </div>
+          </section>
         </div>
       </section>
     )
