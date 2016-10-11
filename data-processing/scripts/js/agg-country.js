82624f935fe7172b6972ff6dce3ad013a740ecbb
Drew Bollinger <drew@developmentseed.org>
2016-10-11 17:34:30 -0400

    Update country aggregation

--- data-processing/scripts/js/agg-country.js
+++ data-processing/scripts/js/agg-country.js
@@ -2,6 +2,7 @@
 
 var fs = require('fs')
 var aggregate = require('geojson-polygon-aggregate')
+var flatten = require('geojson-flatten')
 
 console.log('Aggregating data from: ' + process.argv[3])
 var group = JSON.parse(fs.readFileSync(process.argv[2]))
@@ -12,9 +13,13 @@ var aggregator = {}
 var prependor = (process.argv.length > 5 ? process.argv[5] : '')
 data.features.map(feature => {
   Object.keys(feature.properties).forEach(key => {
-    if (feature.properties[key] > 0 && !aggregator.hasOwnProperty(key)) {
-      // only capture numeric key elements, 'AAL', or things that already have this "style"
-      aggregator[prependor + key.match(/[A-Z]{2}_[A-Z]{2}(_\d+)?|\d+|AAL/)[0]] = function (s, feat) {
+    var match = key.match(/[A-Z]{2}_[A-Z]{2}(_\d+)?|(\d+)|AAL/)
+    if (key.match('HZ')) {
+      match = false
+    }
+    if (feature.properties[key] > 0 && !aggregator.hasOwnProperty(key) && match) {
+      // only capture numeric keys, 'AAL', or things that already have this "style"
+      aggregator[prependor + match[0]] = function (s, feat) {
         return (s || 0) + (Number(feat.properties[key]) || 0)
       }
     }
@@ -23,5 +28,5 @@ data.features.map(feature => {
 console.log('Capturing keys: ' + Object.keys(aggregator).join(', '))
 console.log('Prepending property names with: ' + prependor)
 
-var result = aggregate.groups(group, data, aggregator)
+var result = aggregate.groups(flatten(group), data, aggregator)
 fs.writeFileSync(process.argv[4], JSON.stringify(result))
--- data-processing/scripts/zsh/agg_country.sh
+++ data-processing/scripts/zsh/agg_country.sh
@@ -1,10 +1,11 @@
 #!/bin/zsh
-for country in ../countries/[A-Z][A-Z].geojson; do
+BASE=../CDRP\ Platform\ Development\ Seed
+for country in countries/[A-Z][A-Z].geojson; do
   echo country: ${country:t:r}
   echo ${country:r}
   ## agg-country.js takes 4 arguments, group boundary, data, output file, string to prepend variable names with
-  node ./agg-country.js ${country:r}_clear.geojson ../raw/earthquake_aal_probabilistic/aal.geojson ${country:r}_agg.geojson LS_EQ_
-  node ./agg-country.js ${country:r}_agg.geojson ../raw/windstorm_aal_probabilistic/aal.geojson ${country:r}_agg.geojson LS_WS_
-  node ./agg-country.js ${country:r}_agg.geojson ../raw/flood_aal_probabilistic/aal.geojson ${country:r}_agg.geojson LS_FL_
-  node ./agg-country.js ${country:r}_agg.geojson ${country:r}_grid.geojson ${country:r}_agg.geojson
+  node scripts/js/agg-country.js ${country:r}.geojson $BASE/Loss/Earthquake/probabilistic/aal.geojson ${country:r}_agg.geojson LS_EQ_
+  node scripts/js/agg-country.js ${country:r}_agg.geojson $BASE/Loss/Windstorm/probabilistic/aal.geojson ${country:r}_agg.geojson LS_WS_
+  node scripts/js/agg-country.js ${country:r}_agg.geojson $BASE/Loss/Flood/probabilistic/aal.geojson ${country:r}_agg.geojson LS_FL_
+  node scripts/js/agg-country.js ${country:r}_agg.geojson ${country:r}_grid.geojson ${country:r}_agg.geojson
 done
