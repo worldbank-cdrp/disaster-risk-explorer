aeedcd6e957341c851bed123abb7ab7d46fdf3ad
Drew Bollinger <drew@developmentseed.org>
2016-10-14 09:02:56 -0400

    Fix hazard naming

--- app/assets/scripts/utils/data-selection.js
+++ app/assets/scripts/utils/data-selection.js
@@ -65,7 +65,7 @@ export function DataSelectionFactory (availableParams) {
 let availableParams = {
   metric: [
     {key: 'loss', value: 'Loss'},
-    {key: 'hazard', value: 'Hazard'},
+    {key: 'risk', value: 'Hazard'},
     {key: 'exposure', value: 'Exposure'}
   ],
   risk: [
