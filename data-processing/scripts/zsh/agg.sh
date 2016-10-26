27780229fa7df8a0f9fd6ce9b7d0b6f8338f660b
Drew Bollinger <drew@developmentseed.org>
2016-10-26 16:51:47 -0400

    Update to new normalization measures

--- data-processing/scripts/zsh/agg.sh
+++ data-processing/scripts/zsh/agg.sh
@@ -14,18 +14,12 @@ for country in countries/NI.geojson; do
        scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   ### LOSS
-  scripts/js/agg-loss.js $BASE/Loss/Earthquake/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_EQ_
-  scripts/js/agg-loss.js $BASE/Loss/Windstorm/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_WS_
-  scripts/js/agg-loss.js $BASE/Loss/Windstorm/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_WS_
-  scripts/js/agg-loss.js $BASE/Loss/Flood/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_FL_
+  scripts/js/agg.js $BASE/Loss/Earthquake/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_EQ_
+  scripts/js/agg.js $BASE/Loss/Windstorm/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_WS_
+  scripts/js/agg.js $BASE/Loss/Windstorm/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_WS_
+  scripts/js/agg.js $BASE/Loss/Flood/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_FL_
   ### EXPOSURE
-  for geo in $BASE/Exposure/GDP/${country:t:r}_gdp.geojson; do
-       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
-  done
-  for geo in $BASE/Exposure/Infrastructure/${country:t:r}_infrastructure.geojson; do
-       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
-  done
-  for geo in $BASE/Exposure/Building\ Stock/${country:t:r}_building_stock.geojson; do
-       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
-  done
+  scripts/js/agg.js $BASE/Exposure/GDP/${country:t:r}_gdp.geojson countries/${country:t:r}_grid.geojson
+  scripts/js/agg.js $BASE/Exposure/Infrastructure/${country:t:r}_infrastructure.geojson countries/${country:t:r}_grid.geojson
+  scripts/js/agg.js $BASE/Exposure/Building\ Stock/${country:t:r}_building_stock countries/${country:t:r}_grid.geojson
 done
--- data-processing/scripts/zsh/normalize.sh
+++ data-processing/scripts/zsh/normalize.sh
@@ -2,23 +2,29 @@
 
 ### HAZARD
 ## Rename
-$0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake/probabilistic
+# $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake/probabilistic
 # $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm/probabilistic
 # $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Flood/probabilistic
 
-## Polygonize & clip
+## Raster to polygon & clip
 for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake/probabilistic/*.tif; do
-    gdal_polygonize.py ${tif} -f "GeoJSON" ${tif/tif/geojson} fieldname "risk"
+    echo "normalizing ${tif}"
+    gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
+    scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
 done
-# for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm/probabilistic/*.tif; do
-#     gdal_polygonize.py ${tif} -f "GeoJSON" ${tif/tif/geojson} fieldname "risk"
-#     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
-# done
-# for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Flood/probabilistic/*.tif; do
-#     gdal_polygonize.py ${tif} -f "GeoJSON" ${tif/tif/geojson} fieldname "risk"
-#     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
-# done
+for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm/probabilistic/*.tif; do
+    echo "normalizing ${tif}"
+    gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
+    scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
+    ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
+done
+for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Flood/probabilistic/*.tif; do
+    echo "normalizing ${tif}"
+    gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
+    scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
+    ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
+done
 
 ### LOSS
 #
@@ -34,12 +40,12 @@ done
 #
 ## Convert all to geojson + merge if necessary
 
-ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/CA_Earthquake_AAL.geojson $EQ_LOSS/CA_Earthquake_AAL.shp
-ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/Caribbean_Earthquake_AAL.geojson $EQ_LOSS/Caribbean_Earthquake_AAL.shp
-geojson-merge $EQ_LOSS/CA_Earthquake_AAL.geojson $EQ_LOSS/Caribbean_Earthquake_AAL.geojson > $EQ_LOSS/aal.geojson
-ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/aal.geojson $WS_LOSS/CA_CARIB_WS_AAL.shp
-ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/pml.geojson $WS_LOSS/CA_CARIB_WS_PML.shp
-geojson-merge $FL_LOSS/*.geojson > $FL_LOSS/pml.geojson
+# ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/CA_Earthquake_AAL.geojson $EQ_LOSS/CA_Earthquake_AAL.shp
+# ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/Caribbean_Earthquake_AAL.geojson $EQ_LOSS/Caribbean_Earthquake_AAL.shp
+# geojson-merge $EQ_LOSS/CA_Earthquake_AAL.geojson $EQ_LOSS/Caribbean_Earthquake_AAL.geojson > $EQ_LOSS/aal.geojson
+# ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/aal.geojson $WS_LOSS/CA_CARIB_WS_AAL.shp
+# ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/pml.geojson $WS_LOSS/CA_CARIB_WS_PML.shp
+# geojson-merge $FL_LOSS/*.geojson > $FL_LOSS/pml.geojson
 
 # ### EXPOSURE
 # EX=../CDRP\ Platform\ Development\ Seed/Exposure
