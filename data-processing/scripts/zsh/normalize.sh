7266584564bac0aada710889eb7a04438f9aafc8
Drew Bollinger <drew@developmentseed.org>
2016-10-26 18:44:00 -0400

    Add historic normalization

--- data-processing/scripts/zsh/normalize.sh
+++ data-processing/scripts/zsh/normalize.sh
@@ -5,26 +5,72 @@
 # $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake/probabilistic
 # $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm/probabilistic
 # $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Flood/probabilistic
+# $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake/historic
+# $0:h/rename.sh ../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm/historic
+
+EQ_HZ=../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake
+WS_HZ=../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm
+FL_HZ=../CDRP\ Platform\ Development\ Seed/Hazard/Flood
 
 ## Raster to polygon & clip
-for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Earthquake/probabilistic/*.tif; do
+# probabilistic
+for tif in ${EQ_HZ}/probabilistic/*.tif; do
     echo "normalizing ${tif}"
     gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
     scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
 done
-for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Windstorm/probabilistic/*.tif; do
+for tif in ${WS_HZ}/probabilistic/*.tif; do
     echo "normalizing ${tif}"
     gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
     scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
 done
-for tif in ../CDRP\ Platform\ Development\ Seed/Hazard/Flood/probabilistic/*.tif; do
+for tif in ${FL_HZ}/probabilistic/*.tif; do
     echo "normalizing ${tif}"
     gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
     scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
 done
+
+# historic
+
+echo "normalizing historic"
+mv $EQ_HZ/historic/1839.tif $EQ_HZ/historic/GD_1839.tif
+mv $EQ_HZ/historic/1843.tif $EQ_HZ/historic/LC_1843.tif
+
+gdal_translate -of XYZ $EQ_HZ/historic/GD_1839.tif $EQ_HZ/historic/GD_1839.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/LC_1843.tif $EQ_HZ/historic/LC_1843.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/BZ_24696.tif $EQ_HZ/historic/BZ_24696.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/CR_6383.tif $EQ_HZ/historic/CR_6383.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/GT_1902.tif $EQ_HZ/historic/GT_1902.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/HN_13920.tif $EQ_HZ/historic/HN_13920.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/JM_1907s1.tif $EQ_HZ/historic/JM_1907s1.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/NI_7416.tif $EQ_HZ/historic/NI_7416.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/PA_3882.tif $EQ_HZ/historic/PA_3882.xyz
+gdal_translate -of XYZ $EQ_HZ/historic/SV_20460.tif $EQ_HZ/historic/SV_20460.xyz
+
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/GD_1839.xyz $EQ_HZ/historic/GD_1839.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/LC_1843.xyz $EQ_HZ/historic/LC_1843.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/BZ_24696.xyz $EQ_HZ/historic/BZ_24696.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/CR_6383.xyz $EQ_HZ/historic/CR_6383.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/GT_1902.xyz $EQ_HZ/historic/GT_1902.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/HN_13920.xyz $EQ_HZ/historic/HN_13920.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/JM_1907s1.xyz $EQ_HZ/historic/JM_1907s1.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/NI_7416.xyz $EQ_HZ/historic/NI_7416.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/PA_3882.xyz $EQ_HZ/historic/PA_3882.geojson risk
+scripts/js/xyz-to-polygon.js $EQ_HZ/historic/SV_20460.xyz $EQ_HZ/historic/SV_20460.geojson risk
+
+ogr2ogr -f GeoJSON -clipsrc countries/GD.geojson $EQ_HZ/historic/GD_1839_clipped.geojson $EQ_HZ/historic/GD_1839.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/LC.geojson $EQ_HZ/historic/LC_1843_clipped.geojson $EQ_HZ/historic/LC_1843.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/BZ.geojson $EQ_HZ/historic/BZ_24696_clipped.geojson $EQ_HZ/historic/BZ_24696.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/CR.geojson $EQ_HZ/historic/CR_6383_clipped.geojson $EQ_HZ/historic/CR_6383.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/GT.geojson $EQ_HZ/historic/GT_1902_clipped.geojson $EQ_HZ/historic/GT_1902.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/HN.geojson $EQ_HZ/historic/HN_13920_clipped.geojson $EQ_HZ/historic/HN_13920.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/JM.geojson $EQ_HZ/historic/JM_1907s1_clipped.geojson $EQ_HZ/historic/JM_1907s1.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/NI.geojson $EQ_HZ/historic/NI_7416_clipped.geojson $EQ_HZ/historic/NI_7416.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/PA.geojson $EQ_HZ/historic/PA_3882_clipped.geojson $EQ_HZ/historic/PA_3882.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/SV.geojson $EQ_HZ/historic/SV_20460_clipped.geojson $EQ_HZ/historic/SV_20460.geojson
 
 ### LOSS
 #
