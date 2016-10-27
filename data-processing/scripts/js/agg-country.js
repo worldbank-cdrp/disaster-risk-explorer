96837d32aa0e2e22c7740f5adab7766977aaa3ea
Drew Bollinger <drew@developmentseed.org>
2016-10-27 14:00:08 -0400

    Update key matching

--- data-processing/scripts/js/agg-country.js
+++ data-processing/scripts/js/agg-country.js
@@ -9,22 +9,27 @@ var group = JSON.parse(fs.readFileSync(process.argv[2]))
 var data = JSON.parse(fs.readFileSync(process.argv[3]))
 
 // Aggregate all numeric fields from the data
+var allKeys = {}
 var aggregator = {}
 data.features.map(feature => {
   Object.keys(feature.properties).forEach(key => {
-    var match = key.match(/[A-Z]{2}_[A-Z]{2}(_\d+)?|(\d+)|AAL/)
+    if (!allKeys.hasOwnProperty(key)) {
+      allKeys[key] = true
+    }
+    // only capture numeric keys, 'AAL', or things that already have this "style"
+    var match = key.match(/[A-Z]{2}_[A-Z]{2}(_\d+)?|(\d+)|AAL|HS/)
     if (key.match('HZ')) {
       match = false
     }
-    if (feature.properties[key] > 0 && !aggregator.hasOwnProperty(key) && match) {
-      // only capture numeric keys, 'AAL', or things that already have this "style"
-      aggregator[match[0]] = function (s, feat) {
+    if (!isNaN(feature.properties[key]) && !aggregator.hasOwnProperty(key) && match) {
+      aggregator[key] = function (s, feat) {
         return (s || 0) + (Number(feat.properties[key]) || 0)
       }
     }
   })
 })
 console.log('Capturing keys: ' + Object.keys(aggregator).join(', '))
+console.log('All keys: ' + Object.keys(allKeys).join(', '))
 
 var result = aggregate.groups(flatten(group), data, aggregator)
 fs.writeFileSync(process.argv[4], JSON.stringify(result))
--- data-processing/scripts/zsh/agg_country.sh
+++ data-processing/scripts/zsh/agg_country.sh
@@ -1,11 +1,11 @@
 #!/bin/zsh
 BASE=../CDRP\ Platform\ Development\ Seed
-# for country in countries/[A-Z][A-Z].geojson; do
-#   echo country: ${country:t:r}
-#   echo ${country}
-#   ## agg-country.js takes 4 arguments, group boundary, data, output file, string to prepend variable names with
-#   node scripts/js/agg-country.js ${country:r}.geojson ${country:r}_grid.geojson ${country:r}_agg.geojson
-# done
+for country in countries/[A-Z][A-Z].geojson; do
+  echo country: ${country:t:r}
+  echo ${country}
+  ## agg-country.js takes 4 arguments, group boundary, data, output file, string to prepend variable names with
+  node scripts/js/agg-country.js ${country:r}.geojson ${country:r}_grid.geojson ${country:r}_agg.geojson
+done
 
 for province in countries/[A-Z][A-Z]-[A-Z0-9].geojson; do
   echo province: ${province:t:r}
