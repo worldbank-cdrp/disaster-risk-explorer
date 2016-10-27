18dec22cdea59542c1abc89c092c612c52a57c46
Drew Bollinger <drew@developmentseed.org>
2016-10-27 12:41:51 -0400

    Include historic data

--- data-processing/scripts/zsh/agg.sh
+++ data-processing/scripts/zsh/agg.sh
@@ -1,25 +1,37 @@
 #!/bin/zsh
 setopt null_glob
 BASE=../CDRP\ Platform\ Development\ Seed
-for country in countries/NI.geojson; do
+for country in countries/[A-Z][A-Z].geojson; do
   echo country: ${country:t:r}
   ### HAZARD
-  for geo in $BASE/Hazard/Earthquake/probabilistic/${country:t:r}*_clipped.geojson; do
+  # for geo in $BASE/Hazard/Earthquake/probabilistic/${country:t:r}*_clipped.geojson; do
+  #      scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
+  # done
+  # for geo in $BASE/Hazard/Windstorm/probabilistic/${country:t:r}*_clipped.geojson; do
+  #      scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
+  # done
+  # for geo in $BASE/Hazard/Flood/probabilistic/${country:t:r}*_clipped.geojson; do
+  #      scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
+  # done
+  for geo in $BASE/Hazard/Earthquake/historic/${country:t:r}*_clipped.geojson; do
        scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
-  for geo in $BASE/Hazard/Windstorm/probabilistic/${country:t:r}*_clipped.geojson; do
-       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
-  done
-  for geo in $BASE/Hazard/Flood/probabilistic/${country:t:r}*_clipped.geojson; do
+  for geo in $BASE/Hazard/Windstorm/historic/${country:t:r}*_clipped.geojson; do
        scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
   done
   ### LOSS
-  scripts/js/agg.js $BASE/Loss/Earthquake/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_EQ_
-  scripts/js/agg.js $BASE/Loss/Windstorm/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_WS_
-  scripts/js/agg.js $BASE/Loss/Windstorm/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_WS_
-  scripts/js/agg.js $BASE/Loss/Flood/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_FL_
+  # scripts/js/agg.js $BASE/Loss/Earthquake/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_EQ_
+  # scripts/js/agg.js $BASE/Loss/Windstorm/probabilistic/aal.geojson countries/${country:t:r}_grid.geojson LS_WS_
+  # scripts/js/agg.js $BASE/Loss/Windstorm/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_WS_
+  # scripts/js/agg.js $BASE/Loss/Flood/probabilistic/pml.geojson countries/${country:t:r}_grid.geojson LS_FL_
+  for geo in $BASE/Loss/Earthquake/historic/${country:t:r}*.geojson; do
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
+  done
+  for geo in $BASE/Loss/Windstorm/historic/${country:t:r}*.geojson; do
+       scripts/js/agg.js $geo countries/${country:t:r}_grid.geojson
+  done
   ### EXPOSURE
-  scripts/js/agg.js $BASE/Exposure/GDP/${country:t:r}_gdp.geojson countries/${country:t:r}_grid.geojson
-  scripts/js/agg.js $BASE/Exposure/Infrastructure/${country:t:r}_infrastructure.geojson countries/${country:t:r}_grid.geojson
-  scripts/js/agg.js $BASE/Exposure/Building\ Stock/${country:t:r}_building_stock countries/${country:t:r}_grid.geojson
+  # scripts/js/agg.js $BASE/Exposure/GDP/${country:t:r}_gdp.geojson countries/${country:t:r}_grid.geojson
+  # scripts/js/agg.js $BASE/Exposure/Infrastructure/${country:t:r}_infrastructure.geojson countries/${country:t:r}_grid.geojson
+  # scripts/js/agg.js $BASE/Exposure/Building\ Stock/${country:t:r}_building_stock.geojson countries/${country:t:r}_grid.geojson
 done
--- data-processing/scripts/zsh/normalize.sh
+++ data-processing/scripts/zsh/normalize.sh
@@ -14,28 +14,28 @@ FL_HZ=../CDRP\ Platform\ Development\ Seed/Hazard/Flood
 
 ## Raster to polygon & clip
 # probabilistic
-for tif in ${EQ_HZ}/probabilistic/*.tif; do
-    echo "normalizing ${tif}"
-    gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
-    scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
-    ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
-done
-for tif in ${WS_HZ}/probabilistic/*.tif; do
-    echo "normalizing ${tif}"
-    gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
-    scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
-    ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
-done
-for tif in ${FL_HZ}/probabilistic/*.tif; do
-    echo "normalizing ${tif}"
-    gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
-    scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
-    ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
-done
+# for tif in ${EQ_HZ}/probabilistic/*.tif; do
+#     echo "normalizing ${tif}"
+#     gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
+#     scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
+#     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
+# done
+# for tif in ${WS_HZ}/probabilistic/*.tif; do
+#     echo "normalizing ${tif}"
+#     gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
+#     scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
+#     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
+# done
+# for tif in ${FL_HZ}/probabilistic/*.tif; do
+#     echo "normalizing ${tif}"
+#     gdal_translate -of XYZ ${tif} ${tif/tif/xyz}
+#     scripts/js/xyz-to-polygon.js ${tif/tif/xyz} ${tif/tif/geojson} risk
+#     ogr2ogr -f GeoJSON -clipsrc countries/${${tif:t}:0:2}.geojson ${tif:r}_clipped.geojson ${tif/tif/geojson}
+# done
 
 # historic
 
-echo "normalizing historic"
+echo "normalizing historic hazards"
 mv $EQ_HZ/historic/1839.tif $EQ_HZ/historic/GD_1839.tif
 mv $EQ_HZ/historic/1843.tif $EQ_HZ/historic/LC_1843.tif
 
@@ -50,6 +50,12 @@ gdal_translate -of XYZ $EQ_HZ/historic/NI_7416.tif $EQ_HZ/historic/NI_7416.xyz
 gdal_translate -of XYZ $EQ_HZ/historic/PA_3882.tif $EQ_HZ/historic/PA_3882.xyz
 gdal_translate -of XYZ $EQ_HZ/historic/SV_20460.tif $EQ_HZ/historic/SV_20460.xyz
 
+gdal_translate -of XYZ $WS_HZ/historic/CR_joan_1998_pgs.tif $WS_HZ/historic/CR_joan_1998.xyz
+gdal_translate -of XYZ $WS_HZ/historic/GT_mitch_1998_pgs.tif $WS_HZ/historic/GT_mitch_1998.xyz
+gdal_translate -of XYZ $WS_HZ/historic/HN_mitch_1998_pgs.tif $WS_HZ/historic/HN_mitch_1998.xyz
+gdal_translate -of XYZ $WS_HZ/historic/SV_agatha_2010_pgs.tif $WS_HZ/historic/SV_agatha_2010.xyz
+gdal_translate -of XYZ $WS_HZ/historic/NI_felix_2007_pgs.tif $WS_HZ/historic/NI_felix_2007.xyz
+
 scripts/js/xyz-to-polygon.js $EQ_HZ/historic/GD_1839.xyz $EQ_HZ/historic/GD_1839.geojson risk
 scripts/js/xyz-to-polygon.js $EQ_HZ/historic/LC_1843.xyz $EQ_HZ/historic/LC_1843.geojson risk
 scripts/js/xyz-to-polygon.js $EQ_HZ/historic/BZ_24696.xyz $EQ_HZ/historic/BZ_24696.geojson risk
@@ -60,6 +66,12 @@ scripts/js/xyz-to-polygon.js $EQ_HZ/historic/JM_1907s1.xyz $EQ_HZ/historic/JM_19
 scripts/js/xyz-to-polygon.js $EQ_HZ/historic/NI_7416.xyz $EQ_HZ/historic/NI_7416.geojson risk
 scripts/js/xyz-to-polygon.js $EQ_HZ/historic/PA_3882.xyz $EQ_HZ/historic/PA_3882.geojson risk
 scripts/js/xyz-to-polygon.js $EQ_HZ/historic/SV_20460.xyz $EQ_HZ/historic/SV_20460.geojson risk
+
+scripts/js/xyz-to-polygon.js $WS_HZ/historic/CR_joan_1998.xyz $WS_HZ/historic/CR_joan_1998.geojson V10_peakGu
+scripts/js/xyz-to-polygon.js $WS_HZ/historic/GT_mitch_1998.xyz $WS_HZ/historic/GT_mitch_1998.geojson V10_peakGu
+scripts/js/xyz-to-polygon.js $WS_HZ/historic/HN_mitch_1998.xyz $WS_HZ/historic/HN_mitch_1998.geojson V10_peakGu
+scripts/js/xyz-to-polygon.js $WS_HZ/historic/SV_agatha_2010.xyz $WS_HZ/historic/SV_agatha_2010.geojson V10_peakGu
+scripts/js/xyz-to-polygon.js $WS_HZ/historic/NI_felix_2007.xyz $WS_HZ/historic/NI_felix_2007.geojson V10_peakGu
 
 ogr2ogr -f GeoJSON -clipsrc countries/GD.geojson $EQ_HZ/historic/GD_1839_clipped.geojson $EQ_HZ/historic/GD_1839.geojson
 ogr2ogr -f GeoJSON -clipsrc countries/LC.geojson $EQ_HZ/historic/LC_1843_clipped.geojson $EQ_HZ/historic/LC_1843.geojson
@@ -72,26 +84,69 @@ ogr2ogr -f GeoJSON -clipsrc countries/NI.geojson $EQ_HZ/historic/NI_7416_clipped
 ogr2ogr -f GeoJSON -clipsrc countries/PA.geojson $EQ_HZ/historic/PA_3882_clipped.geojson $EQ_HZ/historic/PA_3882.geojson
 ogr2ogr -f GeoJSON -clipsrc countries/SV.geojson $EQ_HZ/historic/SV_20460_clipped.geojson $EQ_HZ/historic/SV_20460.geojson
 
+ogr2ogr -f GeoJSON -clipsrc countries/CR.geojson $WS_HZ/historic/CR_joan_1998_clipped.geojson $WS_HZ/historic/CR_joan_1998.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/GT.geojson $WS_HZ/historic/GT_mitch_1998_clipped.geojson $WS_HZ/historic/GT_mitch_1998.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/HN.geojson $WS_HZ/historic/HN_mitch_1998_clipped.geojson $WS_HZ/historic/HN_mitch_1998.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/SV.geojson $WS_HZ/historic/SV_agatha_2010_clipped.geojson $WS_HZ/historic/SV_agatha_2010.geojson
+ogr2ogr -f GeoJSON -clipsrc countries/NI.geojson $WS_HZ/historic/NI_felix_2007_clipped.geojson $WS_HZ/historic/NI_felix_2007.geojson
+
+
+# caribbean historic windstorm hazards are shapefiles
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_HZ/historic/BZ_iris2001_clipped.geojson $WS_HZ/historic/BZ_iris2001.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_HZ/historic/GD_ivan2004_clipped.geojson $WS_HZ/historic/GD_ivan2004.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_HZ/historic/JM_gilbert1988_clipped.geojson $WS_HZ/historic/JM_gilbert1988.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_HZ/historic/LC_allen1980_clipped.geojson $WS_HZ/historic/LC_allen1980.shp
+
 ### LOSS
 #
-# EQ_LOSS=../CDRP\ Platform\ Development\ Seed/Loss/Earthquake/probabilistic
-# WS_LOSS=../CDRP\ Platform\ Development\ Seed/Loss/Windstorm/probabilistic
-## mv ../CDRP\ Platform\ Development\ Seed/Loss/Flood/probabilistic_\(asset\ loss\) ../CDRP\ Platform\ Development\ Seed/Loss/Flood/probabilistic
-# FL_LOSS=../CDRP\ Platform\ Development\ Seed/Loss/Flood/probabilistic
+EQ_LOSS=../CDRP\ Platform\ Development\ Seed/Loss/Earthquake
+WS_LOSS=../CDRP\ Platform\ Development\ Seed/Loss/Windstorm
+# mv ../CDRP\ Platform\ Development\ Seed/Loss/Flood/probabilistic_\(asset\ loss\) ../CDRP\ Platform\ Development\ Seed/Loss/Flood/probabilistic
+FL_LOSS=../CDRP\ Platform\ Development\ Seed/Loss/Flood
 #
+
+$0:h/rename.sh $EQ_LOSS/historic
+$0:h/rename.sh $WS_LOSS/historic
+
+# # probabilistic
 # ## Polygonize flood tifs
-# for tif in $FL_LOSS/*.tif; do
+# for tif in $FL_LOSS/probabilistic/*.tif; do
 #   gdal_polygonize.py ${tif} -f "GeoJSON" ${tif/tif/geojson} fieldname ${${tif:t:r}:4}
 # done
 #
 ## Convert all to geojson + merge if necessary
 
-# ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/CA_Earthquake_AAL.geojson $EQ_LOSS/CA_Earthquake_AAL.shp
-# ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/Caribbean_Earthquake_AAL.geojson $EQ_LOSS/Caribbean_Earthquake_AAL.shp
-# geojson-merge $EQ_LOSS/CA_Earthquake_AAL.geojson $EQ_LOSS/Caribbean_Earthquake_AAL.geojson > $EQ_LOSS/aal.geojson
-# ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/aal.geojson $WS_LOSS/CA_CARIB_WS_AAL.shp
-# ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/pml.geojson $WS_LOSS/CA_CARIB_WS_PML.shp
-# geojson-merge $FL_LOSS/*.geojson > $FL_LOSS/pml.geojson
+# ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/probabilistic/CA_Earthquake_AAL.geojson $EQ_LOSS/CA_Earthquake_AAL.shp
+# ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/probabilistic/Caribbean_Earthquake_AAL.geojson $EQ_LOSS/probabilistic/Caribbean_Earthquake_AAL.shp
+# geojson-merge $EQ_LOSS/probabilistic/CA_Earthquake_AAL.geojson $EQ_LOSS/probabilistic/Caribbean_Earthquake_AAL.geojson > $EQ_LOSS/probabilistic/aal.geojson
+# ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/probabilistic/aal.geojson $WS_LOSS/probabilistic/CA_CARIB_WS_AAL.shp
+# ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/probabilistic/pml.geojson $WS_LOSS/probabilistic/CA_CARIB_WS_PML.shp
+# geojson-merge $FL_LOSS/probabilistic/*.geojson > $FL_LOSS/probabilistic/pml.geojson
+
+# historic
+
+echo "normalizing historic loss"
+
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/GD_1839.geojson $EQ_LOSS/historic/GD_1839.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/LC_1843.geojson $EQ_LOSS/historic/LC_1843.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/BZ_24696.geojson $EQ_LOSS/historic/BZ_24696.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/CR_6383.geojson $EQ_LOSS/historic/CR_6383.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/GT_1902.geojson $EQ_LOSS/historic/GT_1902.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/HN_13920.geojson $EQ_LOSS/historic/HN_13920.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/JM_1907s1.geojson $EQ_LOSS/historic/JM_1907s1.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/NI_7416.geojson $EQ_LOSS/historic/NI_7416.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/PA_3882.geojson $EQ_LOSS/historic/PA_3882.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $EQ_LOSS/historic/SV_20460.geojson $EQ_LOSS/historic/SV_20460.shp
+
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/BZ_iris2001.geojson $WS_LOSS/historic/BZ_iris2001.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/GD_ivan2004.geojson $WS_LOSS/historic/GD_ivan2004.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/JM_gilbert1988.geojson $WS_LOSS/historic/JM_gilbert1988.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/LC_allen1980.geojson $WS_LOSS/historic/LC_allen1980.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/CR_joan1988.geojson $WS_LOSS/historic/CR_joan1988.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/GT_mitch1998.geojson $WS_LOSS/historic/GT_mitch1998.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/HN_mitch1998.geojson $WS_LOSS/historic/HN_mitch1998.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/SV_agatha2010.geojson $WS_LOSS/historic/SV_agatha2010.shp
+ogr2ogr -f GeoJSON -t_srs crs:84 $WS_LOSS/historic/NI_felix2007.geojson $WS_LOSS/historic/NI_felix2007.shp
 
 # ### EXPOSURE
 # EX=../CDRP\ Platform\ Development\ Seed/Exposure
